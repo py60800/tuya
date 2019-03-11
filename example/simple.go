90c51c076be0db95013549764460079de30b3d77
py60800 <48283967+py60800@users.noreply.github.com>
2019-03-11 11:34:19 +0100

    Add files via upload

--- example/simple.go
+++ example/simple.go
@@ -1,11 +1,13 @@
 package main
+
 import (
-   "github.com/py60800/tuya"
-   "fmt"
-   "os"
+	"fmt"
+	"time"
+	"github.com/py60800/tuya"
 )
-func main(){
-   var conf = `[
+
+func main() {
+	var conf = `[
     {"gwId":"1582850884f3eb30128e",
      "key":"XXXXXXXXXXX",
      "type":"Switch",
@@ -15,21 +17,20 @@ func main(){
      "type":"Switch",
      "name":"sw2" }
      ]`
-  
-   toSet := true
-   if len(os.Args) > 1{
-      toSet = os.Args[1] == "on"
-   }
 
-   dm := tuya.NewDeviceManager(conf)
-   b1,_ := dm.GetDevice("sw1")
-   sw1  := b1.(tuya.Switch)
+	dm := tuya.NewDeviceManager(conf)
+	b1, _ := dm.GetDevice("sw1")
+	sw1 := b1.(tuya.Switch)
+	for i := 0; ; i++ {
+		toSet := (i % 2) != 0
+		t := time.Now()
+		b, err := sw1.SetW(toSet, 5*time.Second)
+		if err != nil {
+			fmt.Println("Set error:", err)
+		} else {
+			fmt.Printf("Set: %v/%v [%v]\n", b, toSet, time.Now().Sub(t))
+		}
+		time.Sleep(2 * time.Second)
+	}
 
-   r, err := sw1.Set(toSet)
-   
-   if err != nil {
-      fmt.Println("exec error:",err)
-   }else{
-      fmt.Println("Success:",r)
-   }
 }
